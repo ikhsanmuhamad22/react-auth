e0a7eb8273cf770f5fddf8d9e2203bc73030ff58
ikhsan <ikhsanmuhamad3885@gmail.com>
2024-12-24 16:21:07 +0700

    add: user login

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,28 +1,59 @@
-import { Form, Link, useSearchParams } from "react-router-dom";
+import {
+  Form,
+  Link,
+  useSearchParams,
+  useActionData,
+  useNavigation,
+} from "react-router-dom";
 
 import classes from "./AuthForm.module.css";
 
 function AuthForm() {
+  const data = useActionData();
+  const navigation = useNavigation();
+
   const [searchParam] = useSearchParams();
   const isLogin = searchParam.get("mode") === "login";
-  console.log(isLogin);
+  const isSubmitting = navigation.state === "submitting";
   return (
     <>
       <Form method="post" className={classes.form}>
         <h1>{isLogin ? "Log in" : "Create a new user"}</h1>
+        {data && data.errors && (
+          <ul>
+            {Object.values(data.errors).map((err) => (
+              <li key={err}>{err}</li>
+            ))}
+          </ul>
+        )}
+        {data && data.message && <p>{data.message}</p>}
         <p>
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" required />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            required
+            defaultValue="ikhsan@gmail.com"
+          />
         </p>
         <p>
           <label htmlFor="image">Password</label>
-          <input id="password" type="password" name="password" required />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            required
+            defaultValue="ikhsanaja"
+          />
         </p>
         <div className={classes.actions}>
           <Link to={`?mode=${isLogin ? "signup" : "login"}`} type="button">
             {isLogin ? "Create new user" : "Login"}
           </Link>
-          <button>Save</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "submitting" : "save"}
+          </button>
         </div>
       </Form>
     </>
